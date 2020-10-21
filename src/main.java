d250f80b60408183552e07bb1474e765c36756b7
Numelen <nonokat@pm.me>
2020-10-21 13:51:37 +0200

    Added system outputs.

--- src/main.java
+++ src/main.java
@@ -7,11 +7,27 @@ import java.util.ArrayList;
 
 public class main {
 
+
     public static void main (String[] arguments) {
+
         IO.setInputOutputType(IO.IOTYPE.CONSOLE, IO.IOTYPE.CONSOLE, true);
 
         // Grab all addons from the addon folder
         ArrayList<Addon> addons = AddonManager.getAddons();
+
+        IO.printSystemMessage("###################### CONFIG #######################");
+        IO.printSystemMessage("...");
+        IO.printEmptyLines(1);
+
+        IO.printSystemMessage("###################### ADDONS #######################");
+        if (addons.isEmpty()) {
+            IO.printSystemMessage("No addons found.");
+        } else {
+            IO.printSystemMessage(addons.size() + " addon(s) found:");
+            for (Addon addon : addons)
+                IO.printSystemMessage(addon.name);
+        }
+        IO.printEmptyLines(1);
 
         // and initialize them
         AddonManager.initializeAddons(addons);
